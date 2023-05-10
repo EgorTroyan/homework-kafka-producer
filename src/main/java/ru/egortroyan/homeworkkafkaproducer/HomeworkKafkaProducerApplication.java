32c055542957e157f97c2d9238f4c88871111b9c
Egor Troyan <troegor@qwep.ru>
2023-05-10 21:03:15 +0300

    init

---
+++ src/main/java/ru/egortroyan/homeworkkafkaproducer/HomeworkKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package ru.egortroyan.homeworkkafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkKafkaProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkKafkaProducerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/egortroyan/homeworkkafkaproducer/HomeworkKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.egortroyan.homeworkkafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkKafkaProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
